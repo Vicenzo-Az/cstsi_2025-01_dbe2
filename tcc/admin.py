5543501ecdc41bcda7f2fe3b563b1c42f3b10b9d
Vicenzo-Az <vicenzo.net@gmail.com>
2025-05-08 17:17:43 -0300

    Updated project

--- tcc/admin.py
+++ tcc/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from tcc import models
 
-# Register your models here.
+
+@admin.register(models.Client)
+class ClientAdmin(admin.ModelAdmin):
+    list_display = 'id', 'first_name', 'last_name', 'email',
+    ordering = '-id',
+    search_fields = 'id', 'first_name', 'last_name',
+    list_per_page = 20
+    list_max_show_all = 200
+    list_editable = 'first_name', 'last_name', 'email',
+    list_display_links = 'id',
