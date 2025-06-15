280f6793337ca1fb7301d0ff2540220047ce9d78
Vicenzo-Az <vicenzo.net@gmail.com>
2025-06-15 16:58:21 -0300

    Added Token and Token refresh

--- api/urls.py
+++ api/urls.py
@@ -2,6 +2,7 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import DataSourceViewSet, DashboardViewSet, AnalysisReportViewSet
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 router = DefaultRouter()
 router.register(r'data-sources', DataSourceViewSet)
@@ -10,4 +11,7 @@ router.register(r'analysis-reports', AnalysisReportViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
+
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
