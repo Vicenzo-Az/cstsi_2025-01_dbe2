24b6e9d1de3cfba3ac77c4c523090a634066d50d
Vicenzo-Az <vicenzo.net@gmail.com>
2025-07-22 18:46:26 -0300

    feat: added deploy to heroku

--- project/settings.py
+++ project/settings.py
@@ -28,14 +28,15 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 
+# SECRET_KEY = os.getenv('SECRET_KEY')
 SECRET_KEY = os.environ.get('SECRET_KEY', 'dev-secret-key-para-local')
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
+# DEBUG = True
 DEBUG = False
 
 ALLOWED_HOSTS = ['*']
-
 
 # Application definition
 
@@ -105,7 +106,6 @@ WSGI_APPLICATION = 'project.wsgi.application'
 # }
 
 # Production database
-
 url = urlparse(os.environ.get('JAWSDB_URL'))
 
 DATABASES = {
