7fd2dcd823e31c156b258463045c9fe66ca44c9e
Vicenzo-Az <vicenzo.net@gmail.com>
2025-07-29 17:00:51 -0300

    chore: deploy to heroku

--- project/settings.py
+++ project/settings.py
@@ -30,8 +30,8 @@ MEDIA_URL = '/media/'
 
 # SECURITY WARNING: keep the secret key used in production secret!
 
-SECRET_KEY = os.getenv('SECRET_KEY')
-# SECRET_KEY = os.environ.get('SECRET_KEY', 'dev-secret-key-para-local')
+# SECRET_KEY = os.getenv('SECRET_KEY')
+SECRET_KEY = os.environ.get('SECRET_KEY', 'dev-secret-key-para-local')
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
